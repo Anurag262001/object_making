4eadfb8ed91fc00a85ac7ad597dbec3db28eac2a
HP <mranuragsharma01@gmail.com>
2021-10-08 00:14:32 +0530

    object making

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    int age;
+    String name;
+
+    public static void main(String[] args){
+Main obj = new Main();
+obj.age=12;
+obj.name="anurag";
+        System.out.println(obj.age + "  "  + obj.name);
+    }
+}
